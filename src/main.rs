e60ecc402b047d97692cdbcfeb2877510615908e
Amar Hakim <amarhakimhamzah@gmail.com>
2026-05-11 19:04:31 +0700

    fix cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
-use std::time;
+use std::{env, thread, time};
 
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
 pub struct UserCreatedEventMessage {
@@ -29,14 +29,16 @@ message);
 }
 
 fn main() {
-    let listener =
-        CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()
-        ).unwrap();
+    let amqp_url = env::var("AMQP_URL").unwrap_or_else(|_| "amqp://guest:guest@localhost:5672".to_string());
+    println!("Using AMQP_URL={}", amqp_url);
+
+    let listener = CrosstownBus::new_queue_listener(amqp_url).unwrap();
 
     _ = listener.listen("user_created".to_owned(), UserCreatedHandler{},
         crosstown_bus::QueueProperties { auto_delete: false, durable: false,
         use_dead_letter: true });
 
     loop {
+        thread::sleep(time::Duration::from_secs(1));
     }
 }
